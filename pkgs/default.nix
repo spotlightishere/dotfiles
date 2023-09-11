3672d5e32b7c575d971b28031ba0bba869c9971b
Spotlight <spotlight@joscomputing.space>
2023-09-10 23:01:07 -0500

    Remove virt-manager fix-up

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,11 +2,4 @@ self: super:
 {
   monaco-powerline = super.callPackage ./monaco-powerline/default.nix {};
   swiftformat = super.callPackage ./swiftformat.nix {};
-
-  # Include gnome-icon-theme to virt-manager to fix broken icons.
-  virt-manager = super.virt-manager.override {
-    preFixup = super.virt-manager.preFixup ''
-      --prefix XDG_DATA_DIRS : "${super.hicolor-icon-theme}/share"
-    '';
-  };
 }
