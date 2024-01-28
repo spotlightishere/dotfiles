e3131881fabc33a6366eedc9fabdf0194c655017
Spotlight <spotlight@joscomputing.space>
2024-01-27 21:54:06 -0600

    swiftformat: 0.53.0 -> 0.53.1

--- pkgs/swiftformat.nix
+++ pkgs/swiftformat.nix
@@ -2,13 +2,13 @@
 
 clangStdenv.mkDerivation rec {
   pname = "swiftformat";
-  version = "0.53.0";
+  version = "0.53.1";
 
   src = fetchFromGitHub {
     owner = "nicklockwood";
     repo = "SwiftFormat";
     rev = version;
-    sha256 = "sha256-E1hfhQyb32Uiie/Vv4WcXVY7BWXxPX476F0+1CzTuWU=";
+    sha256 = "sha256-8G0H3C8ZpLm2XLGmIDfOJcSmLo3COIPQQKoeKd7pNoc=";
   };
 
   nativeBuildInputs = [ swift swiftpm ];
