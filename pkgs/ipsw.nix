22ba870a9d386a3761f9806c59ab6e4566417384
Spotlight <spotlight@joscomputing.space>
2025-04-21 22:08:49 -0500

    ipsw: 3.1.594 -> 3.1.595

--- pkgs/ipsw.nix
+++ pkgs/ipsw.nix
@@ -4,16 +4,16 @@
 # `buildGoModule` currently uses Go 1.23.
 buildGo124Module rec {
   pname = "ipsw";
-  version = "3.1.594";
+  version = "3.1.595";
 
   src = fetchFromGitHub {
     owner = "blacktop";
     repo = "ipsw";
     rev = "v${version}";
-    hash = "sha256-R1jgzQW16jlr/ittnC+hzRHDCvezdVDMRo5xkAKfz1s=";
+    hash = "sha256-EdJfYUCTGTjcnL9XhwYaTX8T2ZTJph9JtWaBznSqbGM=";
   };
 
-  vendorHash = "sha256-Au/a30r/soYisEz9HG0QoLzNAHlNDsT9FdjRB9LDbnY=";
+  vendorHash = "sha256-d+C6AAj39MBMpBBpKap63x06ZEpHqt1odpwbPzwklVg=";
 
   buildInputs = with pkgs; [
     unicorn
