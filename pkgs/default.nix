bb0574d97e5123c0568f2a89b179ee3bdc705277
Spotlight <spotlight@joscomputing.space>
2025-06-17 18:11:03 -0500

    swiftformat: Remove
    Upstream `nixpkgs` now builds `swiftformat` via SwiftPM.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,4 @@
   ipsw = pkgs.callPackage ./ipsw.nix { };
   monaco-powerline = pkgs.callPackage ./monaco-powerline/default.nix { };
   telnet = pkgs.callPackage ./telnet.nix { };
-  swiftformat = pkgs.callPackage ./swiftformat.nix { };
 }
