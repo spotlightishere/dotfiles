43e1d411b4bd491a56cece39690f1cced8083aac
Spotlight <spotlight@joscomputing.space>
2025-07-27 22:46:20 -0400

    linux: Resolve irecovery build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -61,11 +61,17 @@ let
       hash = "sha256-o1EFY/cv+pQrGexvPOwMs5mz9KRcffnloXCQXMzbmDY=";
     };
 
-    libirecovery = updateDevicePackage {
+    libirecovery = (updateDevicePackage {
       name = "libirecovery";
       rev = "638056a593b3254d05f2960fab836bace10ff105";
       hash = "sha256-loIbNSbwiVE8/jDVIbCVReV7ZkEOxIC7g8zPaSbOA3E=";
-    };
+    }).overrideAttrs(old: {
+        # https://github.com/NixOS/nixpkgs/pull/419291/files
+        configureFlags = [
+        "--with-udevrulesdir=${placeholder "out"}/lib/udev/rules.d"
+        ''--with-udevrule=OWNER="root",GROUP="myusergroup",MODE="0660"''
+      ];
+    });
 
     libplist = updateDevicePackage {
       name = "libplist";
