1641052b097e030b8110ebb3d29ac14e55ba9214
Spotlight <spotlight@joscomputing.space>
2025-08-12 11:40:14 -0400

    darwin: Respect Xcode manpage path

--- hosts/darwin/shared/common.nix
+++ hosts/darwin/shared/common.nix
@@ -11,9 +11,30 @@
     ];
     variables.EDITOR = "${pkgs.vim}/bin/vim";
 
-    # Ensure that the macOS-provided paths and man page paths are respected.
-    # See also: https://github.com/nix-darwin/nix-darwin/issues/391
-    etc."zprofile.local".source = ./zprofile.local;
+    etc = {
+      # Ensure that the macOS-provided paths and man page paths are respected.
+      # See also: https://github.com/nix-darwin/nix-darwin/issues/391
+      "zprofile.local".source = ./zprofile.local;
+
+      # macOS `man` (alongside BSD `man`) is a shell script
+      # that shells out to `xcode-select --show-manpaths`:
+      # https://github.com/apple-oss-distributions/man/blob/248fab9b46e4f2de53e002a2c341367c1f156c9e/man/man.sh#L112-L114
+      #
+      # We'd rather not invoke this from a pure flake.
+      # The following is from the output of `--show-manpaths` on macOS 26.0.
+      # We'll assume Xcode is either Xcode.app or Xcode-Beta.app.
+      "manpaths.d/10-xcode".text = ''
+        /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/share/man
+        /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/usr/share/man
+        /Applications/Xcode.app/Contents/Developer/usr/share/man
+        /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man
+
+        /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/share/man
+        /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/usr/share/man
+        /Applications/Xcode-beta.app/Contents/Developer/usr/share/man
+        /Applications/Xcode-beta.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man
+      '';
+    };
   };
 
   # Similarly, we'd like to use zsh.
