079cdbe777ac200c1041a8a9f605847ab932a83f
Spotlight <spotlight@joscomputing.space>
2026-01-19 03:12:26 -0500

    frida-python: 17.6.0

--- pkgs/frida-tools.nix
+++ pkgs/frida-tools.nix
@@ -5,27 +5,26 @@ let
   system = pkgs.stdenvNoCC.hostPlatform.system;
   wheelMetadata = {
     x86_64-linux = {
-      hash = "sha256-aOkASL1R1dka3lyzYUm9pYbEASVwHdh7YTdfRkCqopc=";
+      hash = "sha256-CibyI85qI9i1J+JnIlp4qI6ydJ1kqxA71NxGGMtWVcs=";
       platform = "manylinux1_x86_64";
     };
     aarch64-linux = {
-      hash = "sha256-1a/BOEiwCHL9TeyUcpcCvGCveX/PFGwLyK/Xf5UTLW0=";
+      hash = "sha256-oZi+rvo4RyggCvCD9Lehex7dcJWGHj6EKBUK1E+lWMA=";
       platform = "manylinux2014_aarch64";
     };
     x86_64-darwin = {
-      hash = "sha256-zYOenwaZPFfppCN7r/iR8eQXx1a0J3RM5aFr7XUTdKQ=";
+      hash = "sha256-0D03kvgHrZoovX+/PgN/inzL5M/a2fQ+o+hsES+uQd4=";
       platform = "macosx_10_13_x86_64";
     };
     aarch64-darwin = {
-      hash = "sha256-uiINI54mZz/CxoqguKonm0iNZWyeX6dY+FEuiFX5YSw=";
+      hash = "sha256-cLN6HdC15zYvxP+qv0O23npEJfQgIZwG0hh5kbko2+Y=";
       platform = "macosx_11_0_arm64";
     };
   }.${system} or (throw "Unsupported system: ${system}");
 
   # Our actual frida-python package, the "frida" package.
-  # We upgrade it to 17.5.2.
   frida-python = pkgs.python3Packages.frida-python.overrideAttrs (old: rec {
-    version = "17.5.2";
+    version = "17.6.0";
 
     src = old.src.override {
       inherit version;
@@ -37,15 +36,14 @@ let
 in
 
 # Next, the wrapping frida-tools CLI.
-  # We similarly want to update it.
 pkgs.frida-tools.overridePythonAttrs (old: rec {
-  version = "14.5.0";
+  version = "14.5.1";
 
   src = pkgs.fetchPypi {
     inherit version;
 
     pname = "frida_tools";
-    hash = "sha256-Wdjx0NDGojpaycHcgXp+UiBsiAoR3V3UaWw9948HWZ0=";
+    hash = "sha256-MsIqk2fHkcQSQGhkTybVxvoKhLvdTWbtFq7w6HykxL8=";
   };
 
   # Inject our updated `frida` package.
