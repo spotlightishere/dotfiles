e3b1d543177348e94bbedcc84064e63f2950a0d2
Spotlight <spotlight@joscomputing.space>
2026-09-09 16:10:20 -0500

    frida: 17.18.0

--- pkgs/frida-tools.nix
+++ pkgs/frida-tools.nix
@@ -4,26 +4,26 @@ let
   system = pkgs.stdenvNoCC.hostPlatform.system;
   wheelMetadata = {
     x86_64-linux = {
-      hash = "sha256-0+Qjkxjvi0xUV8Wc3NaEsq+iwPLKIFnfZ/MMlrvhx5w=";
+      hash = "sha256-2lbR6pyvsg5iMmHckdqSo7UCecYBYGVPUBTB6P6TPUQ=";
       platform = "manylinux1_x86_64";
     };
     aarch64-linux = {
-      hash = "sha256-VRuhYdwjGhN8MA7Ya3n+gF+PtZvAtmWCixUWCXLBWtQ=";
+      hash = "sha256-3/w3dcqQyXOtGyBg+DGgj/qkNxATLVR3hbzlsAqLyWk=";
       platform = "manylinux2014_aarch64";
     };
     x86_64-darwin = {
-      hash = "sha256-Ne6ztF41VF7SisT8A/5fPsSIKwYypBbGFbMammd+AUY=";
+      hash = "sha256-o1g97DxNrZ4bNkajgLXXeFxhWosoLGOSipeSgh6xQ5w=";
       platform = "macosx_10_13_x86_64";
     };
     aarch64-darwin = {
-      hash = "sha256-PzgClRZVAPbQ3Z41kidWNqYBKioYE+YrOuFYqwep7tM=";
+      hash = "sha256-jpejOWn0R7q5+cKsOlj65KFNEhVvYLqiiKzwRepxOs0=";
       platform = "macosx_11_0_arm64";
     };
   }.${system} or (throw "Unsupported system: ${system}");
 
   # Our actual frida-python package, the "frida" package.
   frida-python = pkgs.python3Packages.frida-python.overrideAttrs (old: rec {
-    version = "17.17.0";
+    version = "17.18.0";
 
     src = old.src.override {
       inherit version;
